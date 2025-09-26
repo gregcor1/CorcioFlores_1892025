7c3136619a64ce3d42e338ccd18545d18e04af6d
GregCorcio <ia.gregcorcio@ufg.edu.sv>
2025-09-25 19:48:56 -0600

    Arreglado en Postman

--- process.php
+++ process.php
@@ -1,12 +1,28 @@
 <?php
+require("classes/Estudiante.class.php");
+$Estudiante = new Estudiante();
 
-require("classes/estudiantes.class.php");
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    $tipo_peticion = ( isset($_Get["t"]) ? ($_GET["t"]) : null );
 
-$Estudiante = Estudiante();
+    if($tipo_peticion=="selectAll"){
+        $resultado = $Estudiante->obtenerEstudiante();
+    }else{
+        $resultado = [];
+    }
+}elseif($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(isset($_POST["fnac"]) and isset($_POST["idg"])){
+        $resultado = $Estudiante->nuevoEstudiante($_POST["fnac"],$_POST["idg"]);
 
-$resultado = $resultado->obtenerEstudiante();
+    }else{
+        header('HTTP/1.1 400 Bad Request');
+        $resultado = array("Mensaje"=>"Parametros no enviados");
+    }
+}
 
-header("Content-Type: Applicaton/json");
-echos(json_encode($resultado));
+//$resultado = $Estudiante->obtenerEstudiantes();
+
+header("Content-Type: Application/json");
+echo(json_encode($resultado));
 
 ?>
